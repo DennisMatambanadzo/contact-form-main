c8d82e763761be246eb5e23c2385d36c38c3a71d
matambanadzotanakad <49873792+matazdt@users.noreply.github.com>
2024-06-19 18:05:48 +0200

    field validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,54 +3,53 @@ const lastName = document.getElementById("last-name");
 const email = document.getElementById("email");
 const message = document.getElementById("message");
 const checkbox = document.getElementById("check-box");
-const radio = document.getElementById("radio");
+const radio1 = document.querySelector("#general");
+const radio2 = document.querySelector("#support");
 
-const emailError = document.getElementById("email-error");
-const firstNameError = document.getElementById("first-name-error");
-const lastNameError = document.getElementById("last-name-error");
-const messageError = document.getElementById("message-error");
-const checkboxError = document.getElementById("checkbox-error");
-const radioError = document.getElementById("radio-error");
 const submitBtn = document.getElementById("submit-btn");
 const modal = document.getElementById("modal");
 
-email.addEventListener("blur", () => {
-  if (!email.textContent) {
-    emailError.classList.toggle("off");
-    email.classList.toggle("error-input");
-  }
-});
-firstName.addEventListener("blur", () => {
-  if (firstName.textContent == "") {
-    firstNameError.classList.toggle("off");
-    firstName.classList.toggle("error-input");
-  }
-});
-lastName.addEventListener("blur", () => {
-  if (!lastName.textContent) {
-    lastNameError.classList.toggle("off");
-    lastName.classList.toggle("error-input");
-  }
-});
+const nameFields = document.querySelectorAll(".name-field");
 
-message.addEventListener("blur", () => {
-  if (!message.textContent) {
-    messageError.classList.toggle("off");
-    message.classList.toggle("error-input");
-  }
-});
-checkbox.addEventListener("blur", () => {
-  if (!checkbox.textContent) {
-    checkboxError.classList.toggle("off");
-    checkbox.classList.toggle("error-input");
-  }
-});
-radio.addEventListener("DOMContentLoaded", () => {
-  if (!radio.textContent) {
-    radioError.classList.toggle("off");
-    radio.classList.toggle("error-input");
-  }
-});
+let boolean = true;
+
+// email.addEventListener("blur", () => {
+//   if (!email.textContent) {
+//     emailError.classList.toggle("off");
+//     email.classList.toggle("error-input");
+//   }
+// });
+// firstName.addEventListener("blur", () => {
+//   if (firstName.textContent == "") {
+//     firstNameError.classList.toggle("off");
+//     firstName.classList.toggle("error-input");
+//   }
+// });
+// lastName.addEventListener("blur", () => {
+//   if (!lastName.textContent) {
+//     lastNameError.classList.toggle("off");
+//     lastName.classList.toggle("error-input");
+//   }
+// });
+
+// message.addEventListener("blur", () => {
+//   if (!message.textContent) {
+//     messageError.classList.toggle("off");
+//     message.classList.toggle("error-input");
+//   }
+// });
+// checkbox.addEventListener("blur", () => {
+//   if (!checkbox.textContent) {
+//     checkboxError.classList.toggle("off");
+//     checkbox.classList.toggle("error-input");
+//   }
+// });
+// radio.addEventListener("DOMContentLoaded", () => {
+//   if (!radio.textContent) {
+//     radioError.classList.toggle("off");
+//     radio.classList.toggle("error-input");
+//   }
+// });
 
 /*TODO:
   submit form 
@@ -62,5 +61,52 @@ radio.addEventListener("DOMContentLoaded", () => {
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  modal.classList.add("on");
+  validateFields();
+  validateEmail();
+  validateMessage();
+  validateQuery();
+  if (boolean == true) {
+    modal.classList.add("on");
+  }
 });
+
+function validateFields() {
+  nameFields.forEach((input) => {
+    if (input.value.trim() == "") {
+      boolean = false;
+      input.nextElementSibling.classList.remove("off");
+    } else {
+      input.nextElementSibling.classList.add("off");
+      boolean = true;
+    }
+  });
+}
+
+function validateEmail() {
+  if (!email.value.includes("@")) {
+    email.nextElementSibling.classList.remove("off");
+    boolean = false;
+  } else {
+    email.nextElementSibling.classList.add("off");
+    boolean = true;
+  }
+}
+
+function validateMessage() {
+  if (message.value.trim() == "") {
+    message.nextElementSibling.classList.remove("off");
+    boolean = false;
+  } else {
+    message.nextElementSibling.classList.add("off");
+    boolean = true;
+  }
+}
+function validateQuery() {
+  if (!radio1.checked && !radio2.checked) {
+    boolean = false;
+    radio1.parentElement.parentElement.lastElementChild.classList.remove("off");
+  } else {
+    boolean = true;
+    radio1.parentElement.parentElement.lastElementChild.classList.add("off");
+  }
+}
